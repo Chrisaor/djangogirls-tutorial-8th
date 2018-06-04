8fa204eac15d2c1ec35660e5a5002a9b91964f40
chrisaor <pchpch0070@gmail.com>
2018-06-04 21:53:13 +0900

    HTML파일을 불러올 때 render_to_string 사용

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import render_to_string
+
 
 def post_list(request):
-    return HttpResponse('hello world')
+    html = render_to_string('blog/post_list.html')
+    return HttpResponse(html)
