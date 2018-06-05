da07886f9784a2bdbf3b75650ead38b5a3f0bbb6
chrisaor <pchpch0070@gmail.com>
2018-06-05 12:01:59 +0900

    post_list에서 post_detail로 가는 링크 생성

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import post_list, post_detail
 
 urlpatterns = [
-    url(r'^$', post_list),
-    url(r'^(\d+)/', post_detail)
+    url(r'^$', post_list, name='post-list'),
+    url(r'^(\d+)/', post_detail, name='post-detail')
 ]
